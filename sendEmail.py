8586f5386013b6fa0ca44873fed8b5a718ca6cd6
rahulraghuv <rahulraghuv@gmail.com>
2017-01-20 00:26:21 +0530

    Update sendEmail.py

--- sendEmail.py
+++ sendEmail.py
@@ -3,20 +3,20 @@
 import smtplib
 
 
-sender = 'rahulraghuv@gmail.com'
-receivers = ['rahulraghuv@gmail.com']
+sender = ''
+receivers = ['']
 var=(1,2,3,4,5,6)
-message = """From: From Person <rahulraghuv@gmail.com>
-To: To Person <yogendrakumarc@gmail.com>
+message = """From: From Person <>
+To: To Person <>
 Subject: SMTP e-mail test
 
 This is a test e-mail message.
-""",var
+"""
 
 try:
    smtpObj = smtplib.SMTP('imap.gmail.com', 587)
    smtpObj.starttls()
-   smtpObj.login("rahulraghuv@gmail.com","Synchronizedme3")
+   smtpObj.login("","")
    smtpObj.sendmail(sender, receivers, message) 
    smtpObj.quit()        
    print "Successfully sent email"
